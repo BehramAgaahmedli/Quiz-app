0a0206c7e7ad30df58dfd2a6a470f84ee8831ee3
BehramAgaahmedli <108389656+BehramAgaahmedli@users.noreply.github.com>
2022-07-23 14:13:46 -0700

    ilk10 bolumu quiz bolumu

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Quiz;
 use App\Models\Question;
+use App\Models\Answer;
+use App\Models\Result;
+
 class MainController extends Controller
 {
     public function dashboard()
@@ -14,14 +17,14 @@ class MainController extends Controller
         return view('dashboard',['quizzes'=>$quizzes]);
     }
 
-    public function quiz_detail($slug)   
-    {     $c = Quiz::where('slug','=',$slug)->count();
+    public function quiz_detail($id,$slug)   
+    {     $c = Quiz::where('id','=',$id)->where('slug','=',$slug)->count();
 
         if($c!=0)     
          {
-        $quiz = Quiz::where('slug',$slug)->get();
-       
-           return view('quiz_detail',['quiz'=>$quiz]);
+          $quiz= Quiz::where('id','=',$id)->where('slug',$slug)->with('questions','my_result','results','topTen.user')->first();
+         
+           return view('quiz_detail',compact('quiz'));
          }
           else
           {
@@ -29,6 +32,26 @@ class MainController extends Controller
           }
           
     }
+
+
+
+    public function answer_quiz_detail($id,$slug)   
+    {     $c = Quiz::where('id','=',$id)->where('slug','=',$slug)->count();
+
+        if($c!=0)     
+         {
+         $quiz = Quiz::where('id','=',$id)->where('slug','=',$slug)->with('questions','my_result','results','topTen.user')->first();
+       
+           return view('answer_quiz_detail',compact('quiz'));
+         }
+          else
+          {
+              return redirect('/dashboard');
+          }
+          
+    }
+
+
 
     public function quiz($id,$slug)
     {
@@ -49,9 +72,57 @@ class MainController extends Controller
 
     }
 
-    public function edit($id)
+    public function result(Request $request,$id,$slug)
     {
-     
+        $c = Quiz::where('id','=',$id)->where('slug',$slug)->count();
+
+        if($c!=0)     
+         {
+            $all = $request->except('_token');
+           
+            $quiz= Quiz::where('id','=',$id)->where('slug',$slug)->where('final_price','0.00')->get();
+           $questions=Question::where('quiz_id',$id)->get();
+            $e=Question::where('quiz_id',$id)->count();
+           $correct=0;
+
+           foreach($questions as $question):
+
+            Answer::create([
+
+                'user_id'=>auth()->user()->id,
+                'question_id'=>$question['id'],
+                'answer'=>$request->post($question['id'])
+
+            ]);
+           
+            if( $question['correct_answer']===$request->post($question->id) )
+            {
+                $correct+=1;
+            }
+
+
+          
+           endforeach;
+          
+         $point=round((100 / $e) * $correct);
+         $wrong=$e-$correct;
+          Result::create([
+
+            'user_id'=>auth()->user()->id,
+            'quiz_id'=>$quiz[0]['id'],
+            'points'=>$point,
+            'correct'=> $correct,
+            'wrong'=>$wrong
+        ]);
+
+      
+        return redirect()->route('answer.quiz.detail',['id'=>$quiz[0]['id'],'slug'=>$quiz[0]['slug']])->with('status','Başarıyla Quizi Bitirdin. Puanın : '.$point);
+
+         }
+          else
+          {
+              return redirect('/dashboard');
+          }
     }
 
 
